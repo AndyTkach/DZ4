4a0be99c9c6acb2554387ea39ebc3a7de2f50674
AndyTkach <andrutka4@gmai.com>
2023-01-22 14:29:25 +0300

    Все готово

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+int exponentiation = Exponentiation(numberA, numberB);
+
+Console.WriteLine($"{numberA},{numberB} -> {exponentiation}");
